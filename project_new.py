17570071211f6aadfb1b90ac4b4766a836dec9f4
Animesh <ansa0972@colorado.edu>
2019-04-09 22:29:59 -0600

    recent updated

--- project_new.py
+++ project_new.py
@@ -1,8 +1,11 @@
+''' This is the main source code. This performs the function of merging the different YAML files based on the fact that : String Int and Float should merge in a way that the child directory should overlap the parent directory and if some extra terms are present in the parent directory it should also merge, if a common list present in both parent as well as child directory, then it should concat the list of child with the parent directory. if nested loops are present then it should preserve the key words while merging. Here the merging happens in the following condition : suppose dir1, dir1/dir2, dir1/dir2/dir3, dir1/dir2/dir3/dir4 are directories created and we have input yaml files in the directories dir1 dir3 and dir4 then yaml files of dir3 and dir4 should merge and yaml file of dir1 should be ignored as dir2 doesn't have an input yaml file''' 
+
 import os
 import yaml
 import copy
 import sys
 
+# This function does the merging part of the files
 def recursive_copy(target,src):
 	for k, v in src.items():
 		if type(v) == list:
@@ -23,53 +26,56 @@ def recursive_copy(target,src):
 		else:
 			target[k] = copy.copy(v)
 
+# This function is responsible for printing the dictionary returned in YAML format
 def yaml_op(consecutive_path,file_name):
 	for i in range(0,len(consecutive_path)):
 		if i == len(consecutive_path)-1:
 			break
-		with open(consecutive_path[i]+'/'+file_name) as fp:
-			data = yaml.load(fp)
+		if i == 0:
+			with open(consecutive_path[i]+'/'+file_name) as fp:
+				data = yaml.load(fp)
+		else:
+			data=data_1
 		with open(consecutive_path[i+1]+'/'+file_name) as fp:
 			data_1 = yaml.load(fp)
 		recursive_copy(data_1,data)
-		print(yaml.dump(data_1,default_flow_style=False))
+	print("Files After Merging")
+	print(yaml.dump(data_1,default_flow_style=False))
 
+# This function checks for consecutive paths and ignores if not consecutive paths
 def consec_path(path_to_dir,file_name):
-	consecutive_path= []
+	consecutive_path=[]
 	j=0
 	for i in range (0, len(path_to_dir)):
-		if j==1:
-			j=0
-			continue
 		if i ==  len(path_to_dir)-1:
-			if len(path_to_dir[i].split('/'))==len(path_to_dir[i-1].split('/'))+1:
+			if len(path_to_dir[i].split('/'))+1==len(path_to_dir[i-1].split('/')):
 				consecutive_path.append(path_to_dir[i])
 				break
 			break
 		else:
-			if len(path_to_dir[i+1].split('/'))==len(path_to_dir[i].split('/'))+1:
-				print(path_to_dir[i])
-				consecutive_path.append(path_to_dir[i])
-			if len(path_to_dir[i+1].split('/'))==len(path_to_dir[i].split('/')):
-				consecutive_path.append(path_to_dir[i])
-				j=1
-
-
-	if len(consecutive_path) != 0:
-		consecutive_path.reverse()
-		print(consecutive_path)
+			if len(path_to_dir[i+1].split('/'))+1==len(path_to_dir[i].split('/')):
+				if j == 1:
+					consecutive_path.append(path_to_dir[i+1])
+				else:
+					consecutive_path.append(path_to_dir[i])
+					consecutive_path.append(path_to_dir[i+1])
+					j=1
+			else:
+				break
+	if len(consecutive_path) >1:
 		yaml_op(consecutive_path,file_name)
 	else:
 		print("There are no consecutive files to merge")
-		sys.exit()
 
+# This function takes the input from the test environment and then separates then ignores sister directories and creates a list of consecutive directories
 def path_to_direc(file_name):
 	path_to_dir=[]
+	l=0
 	for root, dirs, files in os.walk("."):
-#		print(files)
 		for file in files:
 			if file == file_name:
-				print(file)
-				path_to_dir.append(root)
-	print(path_to_dir)
+				if len(root.split('/')) != l:
+					path_to_dir.append(root)
+					l=len(root.split('/')) 
+	path_to_dir.reverse()
 	consec_path(path_to_dir,file_name)
--- test.py
+++ test.py
@@ -1,36 +1,128 @@
+''' I have created this test environment using my own modules .This takes an argument from the user and checks if the argument provided by the user has entered an yaml extension. If yes then it proceeds to the test environments else it breaks the flow '''
+
 import subprocess
 import os
 import argparse
 import sys
 import project_new
+import shutil
 
+''' This is the first test environment. Here I created the test directories dir1, dir1/dir2, dir1/dir2/dir3, dir1/dir2/dir3/dir4 and dir1/dir2/dir3/dir4a. After creating the directories I created the input.yaml files in the dir1, dir3 and dir4 and 4a. My expected output should combine files in dir3 and dir4 '''
 def test_env1(file_name):
+
+# Creating directories
 	os.makedirs('dir_test1')
 	os.makedirs('dir_test1/dir_test2')
 	os.makedirs('dir_test1/dir_test2/dir_test3/dir_test4')
 	os.makedirs('dir_test1/dir_test2/dir_test3/dir_test4a')
 	print("Successfully created the directories that will be used in the test")
 	print("Creating the YAML files now")
+
+# Creating YAML files
+
 	with open("dir_test1/"+file_name, 'w') as fh:
 		fh.write("size: 2\ncolor: blue")
 	with open("dir_test1/dir_test2/dir_test3/"+file_name, 'w') as fh:
-		fh.write("size: 2\ncolor: red")
+		fh.write("size: 2\ncolor: red\nnames: ['lizard']\ntodo:\n  - vaccum:\n      - priority: high")
 	with open("dir_test1/dir_test2/dir_test3/dir_test4/"+file_name, 'w') as fh:
-		fh.write("size: 5\ncolor: blue\ncount: 7.5")
+		fh.write("size: 5\ncolor: blue\ncount: 7.5\nnames: ['cat','dog']\ntodo:\n  - laundry:\n      - priority: low")
+	with open("dir_test1/dir_test2/dir_test3/dir_test4a/"+file_name, 'w') as fh:
+		fh.write("size: 10\ncolor: purple")
+	print("YAML files created")
+	print("Passing the files in the source code")
+	project_new.path_to_direc(file_name)
+	shutil.rmtree('dir_test1')
+	print("***********************Test Case 1 Passed*******************************************")
+
+''' This is the second test environment. Here I created the test directories dir1, dir1/dir2, dir1/dir2/dir3, dir1/dir2/dir3/dir4 and dir1/dir2/dir3/dir4a. After creating the directories I created the input.yaml files in the dir1, dir2,dir3 , dir4 and dir4a. My expected output should combine files in all the directories'''
+
+def test_env2(file_name):
+
+# Creating Directories
+	os.makedirs('dir_test1')
+	os.makedirs('dir_test1/dir_test2')
+	os.makedirs('dir_test1/dir_test2/dir_test3/dir_test4')
+	os.makedirs('dir_test1/dir_test2/dir_test3/dir_test4a')
+	print("Successfully created the directories that will be used in the test")
+	print("Creating the YAML files now")
+
+# Creating YAML fies
+	with open("dir_test1/"+file_name, 'w') as fh:
+		fh.write("size: 2\ncolor: blue")
+	with open("dir_test1/dir_test2/"+file_name, 'w') as fh:
+		fh.write("size: 2\ncolor: red\nnames: ['lion']\ntodo:\n  - dish-washing :\n      - priority: high")
+	with open("dir_test1/dir_test2/dir_test3/"+file_name, 'w') as fh:
+		fh.write("size: 2\ncolor: red\nnames: ['lizard']\ntodo:\n  - vaccum:\n      - priority: high")
+	with open("dir_test1/dir_test2/dir_test3/dir_test4/"+file_name, 'w') as fh:
+		fh.write("size: 5\ncolor: blue\ncount: 7.5\nnames: ['cat','dog']\ntodo:\n  - laundry:\n      - priority: low")
 	with open("dir_test1/dir_test2/dir_test3/dir_test4a/"+file_name, 'w') as fh:
 		fh.write("size: 2\ncolor: blue")
 	print("YAML files created")
 	print("Passing the files in the source code")
 	project_new.path_to_direc(file_name)
+	shutil.rmtree('dir_test1')
+	print("***********************Test Case 2 Passed*******************************************")
 
+''' This is the third test environment. Here I created the test directories dir1, dir1/dir2, dir1/dir2/dir3, dir1/dir2/dir3/dir4 and dir1/dir2/dir3/dir4a. After creating the directories I created the input.yaml files in the dir1  and dir4, dir4a. My expected output should show that the files should'nt merge each other '''
 
+def test_env3(file_name):
+
+# Creating Directories
+	os.makedirs('dir_test1')
+	os.makedirs('dir_test1/dir_test2')
+	os.makedirs('dir_test1/dir_test2/dir_test3/dir_test4')
+	os.makedirs('dir_test1/dir_test2/dir_test3/dir_test4a')
+	print("Successfully created the directories that will be used in the test")
+	print("Creating the YAML files now")
+
+# Creating YAML Files
+	with open("dir_test1/"+file_name, 'w') as fh:
+		fh.write("size: 2\ncolor: blue")
+	with open("dir_test1/dir_test2/dir_test3/dir_test4/"+file_name, 'w') as fh:
+		fh.write("size: 5\ncolor: blue\ncount: 7.5\nnames: ['cat','dog']\ntodo:\n  - laundry:\n      - priority: low")
+	with open("dir_test1/dir_test2/dir_test3/dir_test4a/"+file_name, 'w') as fh:
+		fh.write("size: 2\ncolor: blue")
+	print("YAML files created")
+	print("Passing the files in the source code")
+	project_new.path_to_direc(file_name)
+	shutil.rmtree('dir_test1')
+	print("***********************Test Case 3 Passed*******************************************")
+
+''' This is the fourth test environment. Here I created the test directories dir1, dir1/dir2, dir1/dir2/dir3, dir1/dir2/dir3/dir4 and dir1/dir2/dir3/dir4a. After creating the directories I created the input.yaml files in the dir1, dir2 and dir4,dir4a. My expected output should show files dont merge'''
+
+def test_env4(file_name):
+
+# Creating Directories
+	os.makedirs('dir_test1')
+	os.makedirs('dir_test1/dir_test2')
+	os.makedirs('dir_test1/dir_test2/dir_test3/dir_test4')
+	os.makedirs('dir_test1/dir_test2/dir_test3/dir_test4a')
+	print("Successfully created the directories that will be used in the test")
+	print("Creating the YAML files now")
+
+# Creating YAML files
+	with open("dir_test1/"+file_name, 'w') as fh:
+		fh.write("size: 2\ncolor: blue")
+	with open("dir_test1/dir_test2/"+file_name, 'w') as fh:
+		fh.write("size: 2\ncolor: red\nnames: ['lion']\ntodo:\n  - dish-washing :\n      - priority: high")
+	with open("dir_test1/dir_test2/dir_test3/dir_test4/"+file_name, 'w') as fh:
+		fh.write("size: 5\ncolor: blue\ncount: 7.5\nnames: ['cat','dog']\ntodo:\n  - laundry:\n      - priority: low")
+	with open("dir_test1/dir_test2/dir_test3/dir_test4a/"+file_name, 'w') as fh:
+		fh.write("size: 2\ncolor: blue")
+	print("YAML files created")
+	print("Passing the files in the source code")
+	project_new.path_to_direc(file_name)
+	shutil.rmtree('dir_test1')
+	print("***********************Test Case 4 Passed*******************************************")
+
+# This function checks if the file name has proper extension
 def check_filename(file_name):
 	fh,extn= file_name.split('.')
 	if extn=='yaml' or extn=='yml':
 		return 1
 	else:
 		return 0
-
+# Main Function 
 if __name__=="__main__":
 	parser=argparse.ArgumentParser()
 	if len(sys.argv)==1:
@@ -45,3 +137,7 @@ if __name__=="__main__":
 	else:
 		print("File extension is correct and creating the sample test environments")
 		test_env1(file_name)
+		test_env2(file_name)
+		test_env3(file_name)
+		test_env4(file_name)
+
